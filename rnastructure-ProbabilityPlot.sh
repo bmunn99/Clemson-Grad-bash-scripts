92b1e674135cc12b8855abc51ae8b5042e4607cd
bmunn99 <115659013+bmunn99@users.noreply.github.com>
2023-10-05 14:04:29 -0400

    Create rnastructure-ProbabilityPlot.sh

---
+++ rnastructure-ProbabilityPlot.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+#SBATCH --job-name=prob_plot
+#SBATCH -n 20 
+#SBATCH --partition=bigmem
+#SBATCH --time=3:00:00
+#SBATCH --mem=100gb
+#SBATCH --output=/data2/lackey_lab/shalam/rnastructure/partition/logs/prob.%j.out
+#SBATCH --error=/data2/lackey_lab/shalam/rnastructure/partition/logs/prob.%j.err
+#SBATCH --mail-type=all
+#SBATCH --mail-user=shalam@g.clemson.edu
+
+module load rnastructure
+ProbabilityPlot ../pfs/AGO3_WT.pfs ../dp/AGO3_WT.dp -t
+
+ProbabilityPlot ../pfs/AGO3_E638A.pfs ../dp/AGO3_E638A.dp -t
+
+ProbabilityPlot ../pfs/AGO3_P2.pfs ../dp/AGO3_P2.dp -t
+
+ProbabilityPlot ../pfs/AGO3_P1.pfs ../dp/AGO3_P1.dp -t
+
+ProbabilityPlot ../pfs/AGO2_WT.pfs ../dp/AGO2_WT.dp -t
