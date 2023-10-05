a8b975026f23f92e4a63bad427f8ed70bd962671
bmunn99 <115659013+bmunn99@users.noreply.github.com>
2023-10-05 13:54:09 -0400

    Create featcounts.sh

---
+++ featcounts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=P2_ctrls_featcounts
+#SBATCH -n 16
+#SBATCH --partition=compute
+#SBATCH --time=168:00:00
+#SBATCH --mem=20G
+#SBATCH --mail-type=all
+#SBATCH --output=/data2/lackey_lab/shalam/nextseq/logs/featcounts.%j.out
+#SBATCH --error=/data2/lackey_lab/shalam/nextseq/logs/featcounts.%j.err
+#SBATCH --mail-user=shalam@g.clemson.edu
+#path
+cd /data2/lackey_lab/shalam/nextseq/Sept08_23_NextSeq_gBlock_AGO3/bam/P2_male
+#mods
+module add subread/1.6.4
+#feat
+featureCounts -T 2 -t transcript -p -s 2 -g gene_id -Q 30 -F GTF -a /data2/lackey_lab/GenomeReferences/hisat_index/gencode.v39.annotation.gtf  -o P2_three_ctrls_transcript.txt *.bam
