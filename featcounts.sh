c51090c778ef3f9c77e16109ca24bd0a32497604
bmunn99 <115659013+bmunn99@users.noreply.github.com>
2023-10-10 10:37:15 -0400

    Update featcounts.sh

--- featcounts.sh
+++ featcounts.sh
@@ -7,10 +7,13 @@
 #SBATCH --mail-type=all
 #SBATCH --output=/data2/lackey_lab/shalam/nextseq/logs/featcounts.%j.out
 #SBATCH --error=/data2/lackey_lab/shalam/nextseq/logs/featcounts.%j.err
-#SBATCH --mail-user=shalam@g.clemson.edu
-#path
+#SBATCH --mail-user=your_email
+
+# Change directory to where you'll access the bam files
 cd /data2/lackey_lab/shalam/nextseq/Sept08_23_NextSeq_gBlock_AGO3/bam/P2_male
-#mods
+
+# Load the subread module
 module add subread/1.6.4
-#feat
+
+# Run featureCounts
 featureCounts -T 2 -t transcript -p -s 2 -g gene_id -Q 30 -F GTF -a /data2/lackey_lab/GenomeReferences/hisat_index/gencode.v39.annotation.gtf  -o P2_three_ctrls_transcript.txt *.bam
