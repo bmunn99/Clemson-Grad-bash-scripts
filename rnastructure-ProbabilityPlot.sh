f726e19a27c837957220f10d37c75f090bf5aad8
bmunn99 <115659013+bmunn99@users.noreply.github.com>
2023-10-10 10:31:53 -0400

    Update rnastructure-ProbabilityPlot.sh

--- rnastructure-ProbabilityPlot.sh
+++ rnastructure-ProbabilityPlot.sh
@@ -8,9 +8,12 @@
 #SBATCH --output=/data2/lackey_lab/shalam/rnastructure/partition/logs/prob.%j.out
 #SBATCH --error=/data2/lackey_lab/shalam/rnastructure/partition/logs/prob.%j.err
 #SBATCH --mail-type=all
-#SBATCH --mail-user=shalam@g.clemson.edu
+#SBATCH --mail-user=your_email
 
+# Load the rnastructure module
 module load rnastructure
+
+# Run the ProbabilityPlot function to generate dot plots
 ProbabilityPlot ../pfs/AGO3_WT.pfs ../dp/AGO3_WT.dp -t
 
 ProbabilityPlot ../pfs/AGO3_E638A.pfs ../dp/AGO3_E638A.dp -t
