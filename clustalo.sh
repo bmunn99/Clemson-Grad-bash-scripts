e17d98598e8dc567f2f166df9e52a16d8bc06cd3
bmunn99 <115659013+bmunn99@users.noreply.github.com>
2023-10-10 10:38:17 -0400

    Update clustalo.sh

--- clustalo.sh
+++ clustalo.sh
@@ -7,11 +7,15 @@
 #SBATCH --output=/data2/lackey_lab/shalam/PHYL/logs/aln.%j.out
 #SBATCH --error=/data2/lackey_lab/shalam/PHYL/logs/aln.%j.err
 #SBATCH --mail-type=all
-#SBATCH --mail-user=shalam@g.clemson.edu
-# Load software
+#SBATCH --mail-user=your_email
+
+# Change directory to where you'll run your alignment
 cd /data2/lackey_lab/shalam/PHYL/three/test4
-#load software
+
+# Load the conda environment containing the clustalo module
+# If you don't have a conda environment, make one here: https://conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html#activating-an-environment
 source /opt/ohpc/pub/Software/anaconda3/etc/profile.d/conda.sh
 conda activate ago
+
 # Clustalo alignment
 clustalo -i AGO3_refseq_protein.fa -o AGO3_short --outfmt=clu
