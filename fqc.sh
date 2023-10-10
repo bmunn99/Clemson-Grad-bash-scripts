a2fba67f17c809e7f2897c8ca521728699c88150
bmunn99 <115659013+bmunn99@users.noreply.github.com>
2023-10-10 10:36:27 -0400

    Update fqc.sh

--- fqc.sh
+++ fqc.sh
@@ -7,11 +7,15 @@
 #SBATCH --mail-type=all
 #SBATCH --output=/data2/lackey_lab/shalam/nextseq/logs/fqc.%j.out
 #SBATCH --error=/data2/lackey_lab/shalam/nextseq/logs/fqc.%j.err
-#SBATCH --mail-user=shalam@g.clemson.edu
-# Load change dir
+#SBATCH --mail-user=your_email
+
+# Change directory to the one you will pull the FASTQs from
 cd /data2/lackey_lab/shalam/nextseq/Sept08_23_NextSeq_gBlock_AGO3/FASTQ
+
+# Load the fastqc module
 ml fastqc
-#load mod
+
+# Run fastqc on your samples in the directory of interest
 for f in *combined*.fastq.gz; do
 	N=$(basename $f .fastq) ;
 	fastqc -t 4 --extract $f ;
