46d07d697d33af6815293aa738558ef1faf9aa8e
bmunn99 <115659013+bmunn99@users.noreply.github.com>
2023-10-10 10:31:21 -0400

    Update rnastructure-ct2dot.sh

--- rnastructure-ct2dot.sh
+++ rnastructure-ct2dot.sh
@@ -8,9 +8,12 @@
 #SBATCH --output=/data2/lackey_lab/shalam/rnastructure/ct2dot/logs/ct2dot.%j.out
 #SBATCH --error=/data2/lackey_lab/shalam/rnastructure/ct2dot/logs/ct2dot.%j.err
 #SBATCH --mail-type=all
-#SBATCH --mail-user=shalam@g.clemson.edu
+#SBATCH --mail-user=your_email
 
+# Load the rnastructure module
 module load rnastructure
+
+# Run the ct2dot function
 ct2dot /data2/lackey_lab/shalam/rnastructure/fold/CT/Nextseq/3_WT.ct 1 ../dot/Nextseq/3_WT.dot
 ct2dot /data2/lackey_lab/shalam/rnastructure/fold/CT/Nextseq/E638A.ct 1 ../dot/Nextseq/E638A.dot
 ct2dot /data2/lackey_lab/shalam/rnastructure/fold/CT/Nextseq/P2.ct 1 ../dot/Nextseq/P2.dot
