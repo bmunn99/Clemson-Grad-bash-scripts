8d75ff05369eb6270fc69d41dd40cba9b348c081
bmunn99 <115659013+bmunn99@users.noreply.github.com>
2023-10-10 10:30:34 -0400

    Update rnastructure-partition.sh

--- rnastructure-partition.sh
+++ rnastructure-partition.sh
@@ -8,9 +8,12 @@
 #SBATCH --output=/data2/lackey_lab/shalam/rnastructure/partition/logs/part.%j.out
 #SBATCH --error=/data2/lackey_lab/shalam/rnastructure/partition/logs/part.%j.err
 #SBATCH --mail-type=all
-#SBATCH --mail-user=shalam@g.clemson.edu
+#SBATCH --mail-user=your_email
 
+# Load rnastructure module
 module load rnastructure
+
+# Run the partition function
 partition /data2/lackey_lab/shalam/rnastructure/shapemapper/FASTA/full/AGO3_WT.fa /data2/lackey_lab/shalam/rnastructure/partition/pfs/AGO3_WT.pfs -d -md 250 -sh /data2/lackey_lab/shalam/rnastructure/shapemapper/AGO3_results/Nextseq/combined/3_WT/AGO3_WT_AGO3_WT_CDS.shape
 
 partition /data2/lackey_lab/shalam/rnastructure/shapemapper/FASTA/full/AGO3_E638A.fa /data2/lackey_lab/shalam/rnastructure/partition/pfs/AGO3_E638A.pfs -d -md 250 -sh /data2/lackey_lab/shalam/rnastructure/shapemapper/AGO3_results/Nextseq/combined/E638A/AGO3_E638A_AGO3_E638A_CDS.shape
