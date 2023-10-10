ec10023e9fe8e44962f5c8155dec699e1bb588f4
bmunn99 <115659013+bmunn99@users.noreply.github.com>
2023-10-10 10:34:00 -0400

    Update iq-tree.sh

--- iq-tree.sh
+++ iq-tree.sh
@@ -7,11 +7,15 @@
 #SBATCH --output=/data2/lackey_lab/shalam/PHYL/logs/tree.%j.out
 #SBATCH --error=/data2/lackey_lab/shalam/PHYL/logs/tree.%j.err
 #SBATCH --mail-type=all
-#SBATCH --mail-user=shalam@g.clemson.edu
-# Load software
+#SBATCH --mail-user=your_email
+
+# Change directory to one you will be working in
 cd /data2/lackey_lab/shalam/PHYL/three/test4
-#load software
+
+# Load conda environment containing iqtree
+# If you don't have a conda environment, make one here: https://conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html#activating-an-environment
 source /opt/ohpc/pub/Software/anaconda3/etc/profile.d/conda.sh
 conda activate ago
-#iqtree
+
+# Run iqtree
 iqtree -s AGO3_short.aln -nt 4 -pre AGO3_iqtree
