8d999c5a86aab151f8740ff0acef8770b4f3fdb6
bmunn99 <115659013+bmunn99@users.noreply.github.com>
2023-10-10 10:30:56 -0400

    Update rnastructure-fold.sh

--- rnastructure-fold.sh
+++ rnastructure-fold.sh
@@ -8,9 +8,12 @@
 #SBATCH --output=/data2/lackey_lab/shalam/rnastructure/fold/logs/fold.%j.out
 #SBATCH --error=/data2/lackey_lab/shalam/rnastructure/fold/logs/fold.%j.err
 #SBATCH --mail-type=all
-#SBATCH --mail-user=shalam@g.clemson.edu
+#SBATCH --mail-user=your_email
 
+# Load the rnastructure module
 module load rnastructure
+
+# Run the Fold function
 Fold /data2/lackey_lab/shalam/rnastructure/shapemapper/FASTA/full/AGO3_WT.fa ../CT/Nextseq/exp/3_WT.ct -d -mfe -md 250 -sh /data2/lackey_lab/shalam/rnastructure/shapemapper/AGO3_results/Nextseq/3_WT/AGO3_WT_AGO3_WT_CDS.shape
 
 Fold /data2/lackey_lab/shalam/rnastructure/shapemapper/FASTA/full/AGO3_E638A.fa ../CT/Nextseq/exp/E638A.ct -d -mfe -md 250 -sh /data2/lackey_lab/shalam/rnastructure/shapemapper/AGO3_results/Nextseq/E638A/AGO3_E638A_AGO3_E638A_CDS.shape
